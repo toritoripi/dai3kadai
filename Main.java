19602ad86907d7d7ace2d9248f06bb31a1a387f4
さいとう <114993632+toritoripi@users.noreply.github.com>
2022-10-14 22:28:18 +0900

    修正②
    変数名を改めて考えました。選手名から出身中学校を出すので playerMap→playerNameToSchool としてみました。

--- Main.java
+++ Main.java
@@ -6,10 +6,10 @@ public interface Main {
     static void main(String[] args) throws Exception {
 
         //選手情報を登録する
-        Map<String, String> playerMap = new HashMap<>();
-        playerMap.put("赤木", "北村");
-        playerMap.put("小暮", "北村");
-        playerMap.put("桜木", "和光");
+        Map<String, String> playerNameToSchool = new HashMap<>();
+        playerNameToSchool.put("赤木", "北村");
+        playerNameToSchool.put("小暮", "北村");
+        playerNameToSchool.put("桜木", "和光");
 
         //標準入力で選手名を受け取る
         System.out.println("選手名を入力してください。");
@@ -17,11 +17,11 @@ public interface Main {
         String player = scanner.nextLine();
 
         //エラーメッセージを表示する
-        if (playerMap.get(player) == null) {
+        if (playerNameToSchool.get(player) == null) {
             throw new Exception(player + "は存在しません。");
         }
 
         //登録されている場合
-        System.out.println("出身は" + playerMap.get(player) + "中学校です。");
+        System.out.println("出身は" + playerNameToSchool.get(player) + "中学校です。");
     }
 }
